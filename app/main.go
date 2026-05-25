0fd375eac3dfde5bb7e4a73fcce30d714d6d7c31
tajpouria <46822325+tajpouria@users.noreply.github.com>
2026-05-25 21:36:51 +0200

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -4,15 +4,23 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
+loop:
 	for {
 		fmt.Print("$ ")
 		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
 		if err != nil {
 			panic("can not read the input")
 		}
-		fmt.Println(command + ": command not found")
+		command = strings.TrimSpace(command[:len(command)-1])
+		switch command {
+		case "exit":
+			break loop
+		default:
+			fmt.Println(command + ": command not found")
+		}
 	}
 }
