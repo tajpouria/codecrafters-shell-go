1a249e91d4e61565e7668cecfbb6cdcc627e8b4c
tajpouria <46822325+tajpouria@users.noreply.github.com>
2026-05-25 21:30:58 +0200

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -7,10 +7,12 @@ import (
 )
 
 func main() {
-	fmt.Print("$ ")
-	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
-	if err != nil {
-		panic("can not read the input")
+	for {
+		fmt.Print("$ ")
+		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		if err != nil {
+			panic("can not read the input")
+		}
+		fmt.Println(command[:len(command)-1] + ": command not found")
 	}
-	fmt.Println(command[:len(command)-1] + ": command not found")
 }
