ad2ad7cf6600525220d3c4d574644effedb1725c
tajpouria <46822325+tajpouria@users.noreply.github.com>
2026-05-25 22:01:05 +0200

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -7,6 +7,23 @@ import (
 	"strings"
 )
 
+const (
+	Exit = "exit"
+	Echo = "echo"
+	Type = "type"
+)
+
+func getType(command string) string {
+	var cmd string
+	switch command {
+	case Exit, Echo, Type:
+		cmd = "is a shell builtin"
+	default:
+		cmd = "not found"
+	}
+	return cmd
+}
+
 func main() {
 	for {
 		fmt.Print("$ ")
@@ -15,12 +32,16 @@ func main() {
 			panic("can not read the input")
 		}
 		command = strings.TrimSpace(command[:len(command)-1])
-		if command == "exit" {
+		if command == "" {
+			continue
+		} else if command == Exit {
 			break
-		} else if strings.HasPrefix(command, "echo ") {
+		} else if strings.HasPrefix(command, fmt.Sprintf("%s ", Echo)) {
 			fmt.Println(command[5:])
+		} else if strings.HasPrefix(command, fmt.Sprintf("%s ", Type)) {
+			fmt.Printf("%s: %s\n", command, getType(command[5:]))
 		} else {
-			fmt.Println(command + ": command not found")
+			fmt.Printf("%s: command not found\n", command)
 		}
 	}
 }
